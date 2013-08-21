388e209a4fec3bef9c1799b2f6ba5d3d3128a852
abdoral <abdoral.neto@gmail.com>
2013-08-21 15:22:35 -0300

    'Esceleto' básico de um código em C

---
+++ corpo.c
@@ -0,0 +1,26 @@
+/*
+ * Técnicas de Progamação para Engenharia 1
+ * Abdoral Timóteo de Sousa Neto - 357488
+ * Lista de exercício N 
+ * Questão M
+ * 
+ * Algoritmo com um "corpo" de um programa simples em C
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+  /*Aqui se declara todas as variáveis que for precisar
+   * caso necessitide de mais volte até aqui e as declare.
+   */ 
+  
+  
+  /*Aqui vai ficar o corpo do teu código, onde você vai
+   * implementar teu algoritmo.
+   */
+  
+  return 0;
+}
